4cadfa41c707054f99a4516f869d3104887cea02
ArkAow <tom.charpentier@outlook.fr>
2024-10-25 15:09:50 +0200

    Ajout de la page de détail de catégorie

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -15,4 +15,17 @@ class CategoryController extends Controller
         $categories = Category::withCount('tracks')->get();
         return view('app.categories.index', compact('categories'));
     }
+
+    /**
+     * Show details on a categorie.
+     */
+    public function show($categoryId)
+    {
+        $category = Category::with('tracks')->findOrFail($categoryId);
+        $tracks = $category->tracks()->with('user', 'week')->paginate(10);
+        return view('app.categories.show', [
+            'category' => $category,
+            'tracks' => $tracks,
+        ]);
+    }
 }
